67f564574f63a0bdcd739159f6187293fa0ce645
Mehardeep88 <42536877+mehardeep88@users.noreply.github.com>
2019-12-31 19:19:24 +0530

    Update julia-rock-paper-scissors.jl

--- julia-rock-paper-scissors.jl
+++ julia-rock-paper-scissors.jl
@@ -13,8 +13,8 @@ while true
        comp_choice = rand(choices)
        println("Computer's choice:"," ","$comp_choice")
        ch_d = Dict("rock" => 1,"paper" => 2, "scissors" => 3)
-       user_ind = ch_d[choice]
-       comp_ind = ch_d[comp_choice]
+       user_ind = get(ch_d, choice)
+       comp_ind = get(ch_d, comp_choice)
        result = [[1,3,2],[2,1,3],[3,2,1],[4,4,4]]
        result_ind = result[user_ind][comp_ind]
        mssge = [ "A Tie!","You won!!","You Lose :(","Invalid input.Please type a valid move"]
