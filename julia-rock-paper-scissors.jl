6cc6fe3c350feb3acfc849b9391031401d7025b2
Mehardeep88 <42536877+mehardeep88@users.noreply.github.com>
2019-12-31 18:07:45 +0530

    Update julia-rock-paper-scissors.jl

--- julia-rock-paper-scissors.jl
+++ julia-rock-paper-scissors.jl
@@ -1,5 +1,12 @@
+println("Welcome to Rock,Paper and Scissors")
+println("Rules:")
+println("Player should choose one between rock,paper and scissors")
+
+println("Rock beats scissors,scissors beat paper and paper beats rock")
+println("Can you beat the computer? ;)")
+
 while true
-       print("make a choice: ")
+       print("Now,make a choice between rock,paper or scissors: ")
        choice = readline()
        
        choices = ["rock","paper","scissors"]
@@ -8,9 +15,9 @@ while true
        ch_d = Dict("rock" => 1,"paper" => 2, "scissors" => 3)
        user_ind = ch_d[choice]
        comp_ind = ch_d[comp_choice]
-       result = [[1,3,2],[2,1,3],[3,2,1]]
+       result = [[1,3,2],[2,1,3],[3,2,1],[4,4,4]]
        result_ind = result[user_ind][comp_ind]
-       mssge = [ "A Tie!","You won!!","You Lose :("]
+       mssge = [ "A Tie!","You won!!","You Lose :(","Invalid input.Please type a valid move"]
        res = mssge[result_ind]
        println("---------",res,"--------")
    end
