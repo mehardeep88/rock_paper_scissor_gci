90ed215128e363df787ec6df5752766207780874
Mehardeep88 <42536877+mehardeep88@users.noreply.github.com>
2020-01-02 16:35:44 +0530

    Update julia-rock-paper-scissors.jl

--- julia-rock-paper-scissors.jl
+++ julia-rock-paper-scissors.jl
@@ -4,22 +4,7 @@ println("Player should choose one between rock,paper and scissors")
 
 println("Rock beats scissors,scissors beat paper and paper beats rock")
 println("Can you beat the computer? ;)")
-for range in 1:3
-       print("Now,make a choice between rock,paper or scissors: ")
-       choice = readline()
-       
-       choices = ["rock","paper","scissors"]
-       comp_choice = rand(choices)
-       println("Computer's choice:"," ","$comp_choice")
-       ch_d = Dict("rock" => 1,"paper" => 2, "scissors" => 3)
-       user_ind = get(ch_d, choice,4)
-       comp_ind = get(ch_d, comp_choice,4)
-       result = [[1,3,2],[2,1,3],[3,2,1],[4,4,4]]
-       result_ind = result[user_ind][comp_ind]
-       mssge = [ "A Tie!","You won!!","You Lose :(","Invalid input.Please type a valid move"]
-       res = mssge[result_ind]
-       println("---------",res,"--------")
-end
+
 pl1 = 0
 pl2 = 0
 while true
