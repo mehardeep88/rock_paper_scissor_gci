f98aefc1f9713785aa184d72fcc34920c4df206a
Mehardeep88 <42536877+mehardeep88@users.noreply.github.com>
2020-01-04 17:26:59 +0530

    Update julia-rock-paper-scissors.jl

--- julia-rock-paper-scissors.jl
+++ julia-rock-paper-scissors.jl
@@ -5,30 +5,35 @@ println("Player should choose one between rock,paper and scissors")
 println("Rock beats scissors,scissors beat paper and paper beats rock")
 println("Can you beat the computer? ;)")
 
-pl1 = 0
-pl2 = 0
-while true
-    global pl1
-    global pl2
-    println("Choose between rock,paper or scissors")
-    print("make a choice: ")
-    choice = readline()
-    choices = ["rock","paper","scissors"]
-    comp_choice = rand(choices)
-    println("Computer's choice:"," ","$comp_choice")
-    ch_d = Dict("rock" => 1,"paper" => 2, "scissors" => 3)
-    
-    user_ind = get(ch_d, choice, 4)
-    comp_ind = get(ch_d, comp_choice,4)
-    result = [[1,3,2],[2,1,3],[3,2,1],[4,4,4]]
-    result_ind = result[user_ind][comp_ind]
-    mssge = [ "A Tie!","You won!!","You Lose :(","Invalid"]
-    res = mssge[result_ind]
-    println("---------",res,"--------")
-        (res == mssge[2]) ? pl1 += 1 : pl1 += 0
-        (res == mssge[3]) ? pl2 += 1 : pl2 += 0
+
+
+function game()
+    pl1 = 0
+    pl2 = 0
+    while true
+        println("Choose between rock,paper or scissors")
+        print("make a choice: ")
+        choice = readline()
+        choices = ["rock","paper","scissors"]
+        comp_choice = rand(choices)
+        println("Computer's choice:"," ","$comp_choice")
+        ch_d = Dict("rock" => 1,"paper" => 2, "scissors" => 3)
+
+        user_ind = get(ch_d, choice, 4)
+        comp_ind = get(ch_d, comp_choice,4)
+        result = [[1,3,2],[2,1,3],[3,2,1],[4,4,4]]
+        result_ind = result[user_ind][comp_ind]
+        mssge = [ "A Tie!","You won!!","You Lose :(","Invalid"]
+        res = mssge[result_ind]
+        println("---------",res,"--------")
+            (res == mssge[2]) ? pl1 += 1 : pl1 += 0
+            (res == mssge[3]) ? pl2 += 1 : pl2 += 0
 pl1 == 2 || pl2 == 2 ? break : continue
+    end
 end
+
+
+game()
 
 
 
