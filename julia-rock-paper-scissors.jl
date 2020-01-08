905d245ae54af4c1e91f288b8325a724e7d392d3
Mehardeep88 <42536877+mehardeep88@users.noreply.github.com>
2020-01-08 06:50:48 +0530

    Update julia-rock-paper-scissors.jl

--- julia-rock-paper-scissors.jl
+++ julia-rock-paper-scissors.jl
@@ -18,7 +18,7 @@ function rps()
         comp_choice = rand(choices)
         println("Computer's choice:"," ","$comp_choice")
         ch_d = Dict("rock" => 1,"paper" => 2, "scissors" => 3)
-
+        
         user_ind = get(ch_d, choice, 4)
         comp_ind = get(ch_d, comp_choice,4)
         result = [[1,3,2],[2,1,3],[3,2,1],[4,4,4]]
@@ -35,7 +35,7 @@ end
 end
 
 
-game()
+rps()
 
 
 
